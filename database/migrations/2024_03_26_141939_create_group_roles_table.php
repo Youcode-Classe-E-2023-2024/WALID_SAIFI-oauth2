df122d2a337f5dec5059b5cc6f99867598c30504
Walid Saifi <saifiwalid21@gmail.com>
2024-03-26 15:05:24 +0000

    Update 2024_03_26_141939_create_group_roles_table.php

--- database/migrations/2024_03_26_141939_create_group_roles_table.php
+++ database/migrations/2024_03_26_141939_create_group_roles_table.php
@@ -4,25 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUserGroupRolesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
-        Schema::create('group_roles', function (Blueprint $table) {
+        Schema::create('user_group_roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('group_id');
+
             $table->timestamps();
+
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+
+
+
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('group_roles');
-    }
-};
+}
