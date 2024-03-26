95c7de19d6e3adaeee97506833536270ae21d044
Walid Saifi <saifiwalid21@gmail.com>
2024-03-26 11:50:09 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,21 +20,3 @@ Route::get('/', function () {
 });
 
 
-Route::get('/register',[AuthentificationController::class, 'pageregister'])->name('pageregister');
-
-
-    Route::get('/login',[AuthentificationController::class,'pagelogin'])->name('login');
-    Route::post('/login',[AuthentificationController::class,'loginAction'])->name('loginAction');
-
-
-Route::post('/register.save',[AuthentificationController::class, 'registerSave'])->name('register.save');
-Route::post('/', [AuthentificationController::class, 'destroy'])->name('logout');
-
-
-Route::post('subscribe', [SubscriptionController::class, 'subscribe'])->name('subscribe');
-Route::get('/forget_password', [ForgetpasswordController::class, 'fogetpassword'])->name('foget.password');
-Route::post('/forget_password', [ForgetpasswordController::class, 'fogetpasswordPost'])->name('foget.passwordPost');
-
-Route::get('/rest_password/{token}', [ForgetpasswordController::class, 'rest_password'])->name('rest.password');
-Route::post('/rest_password', [ForgetpasswordController::class, 'rest_passwordPost'])->name('rest.passwordPost');
-
