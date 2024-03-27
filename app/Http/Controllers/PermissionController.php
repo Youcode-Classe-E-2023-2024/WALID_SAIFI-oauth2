adc6db534f5491a3804534830d8ac12abfcd9a57
Walid Saifi <saifiwalid21@gmail.com>
2024-03-27 14:00:39 +0000

    Create PermissionController.php

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Permission;
+
+class PermissionController extends Controller
+{
+    public function index()
+    {
+        $permissions = Permission::all();
+        return response()->json($permissions);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|unique:permissions|string|max:255',
+        ]);
+
+        $permission = Permission::create([
+            'name' => $request->input('name'),
+        ]);
+
+        return response()->json($permission, 201);
+    }
+
+    public function show($id)
+    {
+        $permission = Permission::findOrFail($id);
+        return response()->json($permission);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $permission = Permission::findOrFail($id);
+
+        $request->validate([
+            'name' => 'required|string|max:255|unique:permissions,name,' . $id,
+        ]);
+
+        $permission->name = $request->input('name');
+        $permission->save();
+
+        return response()->json($permission, 200);
+    }
+
+    public function destroy($id)
+    {
+        $permission = Permission::findOrFail($id);
+        $permission->delete();
+
+        return response()->json(['message' => 'Permission supprimée avec succès'], 200);
+    }
+}
