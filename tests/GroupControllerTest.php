75b0f1b2210b9dfdc4b83ed9ffc334d5de826b29
Walid Saifi <saifiwalid21@gmail.com>
2024-03-29 13:52:09 +0000

    Update GroupControllerTest.php

--- tests/GroupControllerTest.php
+++ tests/GroupControllerTest.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class GroupeControllerTest extends TestCase
+class GroupControllerTest extends TestCase
 {
     use RefreshDatabase;
 
